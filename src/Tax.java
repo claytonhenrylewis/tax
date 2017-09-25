1a9c95d9eee981f149642dd3431c6b0abc638b46
claytonhenrylewis <claytonhenrylewis@gmail.com>
2017-09-25 17:02:57 -0400

    Configure input and output

--- src/Tax.java
+++ src/Tax.java
@@ -1,25 +1,45 @@
 import java.io.*;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.ArrayList;
 
 public class Tax {
   public static void main(String [] args) {
-    Item.setSalesTaxRate(.10);
-    Item.setImportTaxRate(.05);
+    if (args.length < 3) {
+      System.out.println("Usage: java -cp .:../bin/stdlib.jar:../bin/algs4.jar Tax [sales-tax-rate] [import-tax-rate] [input-file] [input-file] ...");
+      return;
+    }
+
+    Item.setSalesTaxRate(Double.parseDouble(args[0]));
+    Item.setImportTaxRate(Double.parseDouble(args[1]));
 
     WordNet wordNet = new WordNet("../synsets.txt", "../hypernyms.txt");
     Item.setWordNet(wordNet);
 
     Exemptions exemptions = new Exemptions("../exemptions.txt");
-    exemptions.printExemptions();
     Item.setExemptions(exemptions);
 
-    ItemList list1 = new ItemList("../input1.txt");
-    ItemList list2 = new ItemList("../input2.txt");
-    ItemList list3 = new ItemList("../input3.txt");
+    ArrayList<ItemList> itemLists = new ArrayList<ItemList>();
+    for (int i = 2; i < args.length; i++) {
+      String path = "../input/";
+      path = path.concat(args[i]);
+      itemLists.add(new ItemList(path));
+    }
 
-    System.out.println(list1.printList());
-    System.out.println(list2.printList());
-    System.out.println(list3.printList());
+    for (ItemList itemList : itemLists) {
+      System.out.println(itemList.printList());
+    }
+
+    try {
+      BufferedWriter bw = new BufferedWriter(new FileWriter("../output/output.txt"));
+      for (ItemList itemList : itemLists) {
+          String list = itemList.printList();
+          bw.append(list);
+      }
+      bw.close();
+    } catch (IOException e) {
+      System.out.println("Error writing to output file.");
+      e.printStackTrace();
+    }
   }
 }
